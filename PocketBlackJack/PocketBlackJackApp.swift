b0eecc5d3122cfc3f6c1c1d8a1e2f9271825a5f0
Arnaud Callea <arnaudcallea@gmail.com>
2025-04-13 12:08:38 +0200

    Initial commit

---
+++ PocketBlackJack/PocketBlackJackApp.swift
@@ -0,0 +1,32 @@
+//
+//  PocketBlackJackApp.swift
+//  PocketBlackJack
+//
+//  Created by Arnaud Callea on 13/04/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PocketBlackJackApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PocketBlackJackTests/PocketBlackJackTests.swift
@@ -0,0 +1,16 @@
+//
+//  PocketBlackJackTests.swift
+//  PocketBlackJackTests
+//
+//  Created by Arnaud Callea on 13/04/2025.
+//
+
+import Testing
+
+struct PocketBlackJackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
